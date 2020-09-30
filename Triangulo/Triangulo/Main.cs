2fcd2ff6106e1d2cf9ee8f89552bceda575680d3
Julio Silva <JCesar_060193@hotmail.com>
2020-09-29 21:45:46 -0300

    Primeiro exercicio concluido

---
+++ Triangulo/Triangulo/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Triangulo
+{
+    class Main
+    {
+        static void main(String[] args)
+        {
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Area de X=" + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y=" + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //Imprime na tela o valor encontrado
+
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+            //Condicional
+        }
+    }
+}
---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//Nome: Julio Cesar Henrique e Silva RA: 142150
+//Exercicio: Calcular a area de um triangulo
+
+namespace Triangulo
+{
+    class Program
+    {
+        public double A;
+        public double B;
+        public double C;
+        //Declaração de variaveis
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+            //Calculo para achar a area desejada
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
